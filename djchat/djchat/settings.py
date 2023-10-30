5176b9ffe2bc6755ced629c4ee26b39a96e5d5c8
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-10-30 15:18:02 +0700

    create api endpoint for filtering servers by category and quantity, and add serializer

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -29,6 +29,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external
+    "drf_spectacular",
+    "rest_framework",
+    # internal
     "account",
     "server",
 ]
@@ -117,3 +121,18 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication"
+    ],
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+    # OTHER SETTINGS
+}
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Server
+from .serializer import ServerSerializer
+
+
+class ServerListViewSet(viewsets.ViewSet):
+    queryset = Server.objects.all()
+
+    def list(self, request):
+        category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+
+        if category:
+            self.queryset = self.queryset.filter(category__name=category)
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
+
+        serializer = ServerSerializer(self.queryset, many=True)
+
+        return Response(serializer.data)
