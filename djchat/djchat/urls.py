7fce015abe59b341c49d47aa643326eff5805479
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-10-31 17:53:30 +0700

    add image storing handling

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -1,8 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from server.views import ServerListViewSet
+
 
 router = DefaultRouter()
 router.register("api/server/select", ServerListViewSet)
@@ -12,3 +15,6 @@ urlpatterns = [
     path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/schema/ui/", SpectacularSwaggerView.as_view()),
 ] + router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
