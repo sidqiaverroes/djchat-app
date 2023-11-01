cb509d7fd1313154abf95ae43abc28713c5620dc
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-11-01 17:59:30 +0700

    add initial react routing

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,12 +1,18 @@
+import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from "react-router-dom"
+import Home from "./pages/Home"
 
-
-function App() {
-
-  return (
-    <>
-     
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
   )
+)
+
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
+
+
