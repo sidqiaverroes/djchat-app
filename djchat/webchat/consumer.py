0a7dc5dcf5a77dd4245900c3bbef176252f7a541
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-11-06 08:29:50 +0700

    Multiple user chat rooms

--- djchat/webchat/consumer.py
+++ djchat/webchat/consumer.py
@@ -1,24 +1,31 @@
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class MyConsumer(WebsocketConsumer):
-    # groups = ["broadcast"]
+class WebChatConsumer(JsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.room_name = "testserver"
 
     def connect(self):
-        # Called on connection.
-        # To accept the connection call:
         self.accept()
-        # Or accept the connection and specify a chosen subprotocol.
-        # A list of subprotocols specified by the connecting client
-        # To reject the connection, call:
-        # self.close()
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name,
+        )
 
-    def receive(self, text_data):
-        print(text_data)
-        self.send(text_data=text_data)
-        # Want to force-close the connection? Call:
-        self.close()
+    def receive_json(self, content):
+        print(content["message"])
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_name,
+            {
+                "type": "chat.message",
+                "new_message": content["message"],
+            },
+        )
+
+    def chat_message(self, event):
+        self.send_json(event)
 
     def disconnect(self, close_code):
         pass
-        # Called when the socket closes
--- reactchat/src/pages/Server.tsx
+++ reactchat/src/pages/Server.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import useWebSocket from "react-use-websocket"
 
 const socketUrl = "ws://127.0.0.1:8000/ws/test"
 
-
-
 const Server = () => {
+    const [newMessage, setNewMessage] = useState<string[]>([])
     const [message, setMessage] = useState("")
-    const [inputValue, setInputValue] = useState("")
+
     const { sendJsonMessage } = useWebSocket(socketUrl, {
         onOpen: () => {
             console.log("Connected!")
@@ -19,22 +18,38 @@ const Server = () => {
             console.log("Error!")
         },
         onMessage: (msg) => {
-            setMessage(msg.data)
+            console.log("Received message:", msg);
+            const data = JSON.parse(msg.data)
+            setNewMessage(prev_msg => [...prev_msg, data.new_message])
         }
     })
 
-    const sendInputValue = () => {
-        const message = {text: inputValue}
-        sendJsonMessage(message)
-        setInputValue("")
-    }
+    const handleSendMessage = () => {
+        console.log("Sending message:", message);
+        sendJsonMessage({ type: "message", message });
+    };
+    
+    useEffect(()=>{
+        console.log(newMessage)
+    }, [newMessage])
 
     return (
         <div>
-            <input type="text" value={inputValue} onChange={(e)=> setInputValue(e.target.value)} />
-            <button onClick={sendInputValue}>Send Message</button>
-            <div> Recieved data: {message} </div>
-            
+            CHAT ROOM
+            {newMessage.map((msg, index) => {
+                console.log(msg)
+                return(
+                    <div key={index}>
+                        <p>{msg}</p>
+                    </div>
+                );
+            })}
+            <form onSubmit={(e) => e.preventDefault()}>
+                <label>Enter Message:
+                    <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+                </label>
+            <button onClick={handleSendMessage}>Send Message</button>
+            </form>
         </div>
     )
 }
